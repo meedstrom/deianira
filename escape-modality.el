80561ae166264cfb3acb459ebd1edd7039c282ae
Martin Edström <meedstrom@teknik.io>
2019-09-11 13:28:29 +0200

    Add GPL blurbs

--- escape-modality.el
+++ escape-modality.el
@@ -1,16 +1,28 @@
 ;; escape-modality.el -- indescribable as yet
+;; Copyright (C) 2019 Martin Edstrom
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;; warnings: odd behavior if a head that calls another hydra is not set to exit
 
 (require 'hydra)
-(require 'escape-modality-sparsemap)
+(require 'escape-modality-common)
 (require 'escape-modality-x11)
+(require 'escape-modality-sparsemap)
 (eval-when-compile 'subr) ;; split-string
 (eval-when-compile (require 'subr-x)) ;; string-join
 
-;; Compact ways to get a list
-(mapcar #'char-to-string "sdfsdfds")
-(split-string "sadfsafas" "" t)
 ;;;;;;;;;;;;;;;
 ;;; Micro level
 
@@ -19,10 +31,6 @@
   (let ((optimal (- (round (frame-parameter nil 'width) 10) 4)))
     (max optimal 8)))
 
-(defvar esm-hydra-keys "1234567890qwertyuiopasdfghjkl;zxcvbnm,./")
-
-(defun esm-get-hydra-keys ()
-  (split-string esm-hydra-keys "" t))
 
 (defvar esm-quitters '("C-q" "C-s" "C-g" "C-u" "M-x" "M-u" "s-u" "C-2" "C-c c"))
 
@@ -308,7 +316,7 @@ results in your init file and set the variable directly."
 
 ;; Stage 3
 ;; (esm-super-map-from-ctl-meta)
-(esm-super-translate-to-ctl-meta)
+;; (esm-super-translate-to-ctl-meta)
 
 ;; Stage 4
 
